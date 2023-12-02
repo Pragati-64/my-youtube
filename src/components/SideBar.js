a696adb7f200bb57a96d3f295d368cb16a3fbe4d
Pragati-64 <pragatigupta234@gmail.com>
2023-12-02 16:50:41 +0530

    Adding SideBar lists

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,7 +2,49 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className='p-5 w-48 shadow-md'>
+
+      <div>
+        <ul>
+          <li className='flex'> <img src='/images/home.png' className='h-6 p-1'/>Home</li>
+          <li className='flex'> <img src='/images/clock.png' className='h-6 p-1'/> Shorts</li>
+          <li>Subscriptions</li>
+        </ul>
+      </div>
+
+      <div>
+      <h1 className='font-bold pt-4'>You</h1>
+        <ul>
+          <li>Your channel</li>
+          <li>History</li>
+          <li>Your videos</li>
+          <li>Watch later</li>
+          <li>Show More</li>
+        </ul>
+      </div>
+    <div>
+    <h1 className='font-bold pt-4'>Subscriptions</h1>
+      <ul>
+        <li>Abdul Bari</li>
+        <li>Aditya Verma</li>
+        <li>Akshay Saini</li>
+        <li>Applicaion Club</li>
+      </ul>
+    </div>
+
+    <div>
+    <h1 className='font-bold pt-4'>Explore</h1>
+      <ul>
+        <li>Trending</li>
+        <li>Shopping</li>
+        <li>Music</li>
+        <li>Movies</li>
+        <li>Gaming</li>
+        <li>News</li>
+      </ul>
+    </div>
+      
+    </div>
   )
 }
 
