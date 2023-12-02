1ed5a5cf152f4af709674db8f5cb91bb8d16ec2f
Pragati-64 <pragatigupta234@gmail.com>
2023-12-02 14:46:17 +0530

    Initial setup with skeleton

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
+
+const App = () =>{
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+export default App;
+
+// App 
+//     Header 
+//         Logo 
+//         Search
+//         UserIcon
+//     Body 
+//         SideBar 
+//             MenuItems
+//         MainContainer 
+//             ButtonList 
+//             VideoContainer
+//                 VideoCard
