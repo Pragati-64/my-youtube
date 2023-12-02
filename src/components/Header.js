6af502b91937240fde8713497a308a4ace4f0133
Pragati-64 <pragatigupta234@gmail.com>
2023-12-02 15:42:07 +0530

    Adding logo search user in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='grid grid-flow-col m-4'>
+      <div className='flex'>
+        <img alt='hamburger-menu-icon' src="/images/hamburger-menu.png" className='h-8' />
+        <img alt='youtube-logo' src="/images/youtube-logo.png" className='h-8' />
+      </div>
+      <div>
+        <input type="text" placeholder='Search' />
+        <button>Search</button>
+      </div>
+      <div>
+        <img alt="profile-user-icon" src="/images/profile-user.png" className='h-8' />
+      </div>
+    </div>
   )
 }
 
